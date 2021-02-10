ebf6f652087c0ce9d8839556885c7c2de4aa05b5
nrodarte <70286040+nrodarte@users.noreply.github.com>
2021-02-09 19:33:19 -0800

    work

---
+++ lab4/js/lab.js
@@ -0,0 +1,60 @@
+//How to Cook traditional  Spagehtti Aglio e Olio: 15 minutes, 6 ingriendients
+//1. To cook traditional Aglio e Olio (oil and garlic) you only need to gather a few things
+  //gather the following ingriendients: Extra Virgin Olive Oil, Fresh Garlic, Dried or Fresh Spagehtti, Salt, and Italian Parsely, dried red chili flakes.
+    //optionally: pecorino romano/ parmagianno regianno cheeses, teaspoon of fresh lemon juice, black pepper
+      //gather the following items: pot for boiling water, a decent sized pan, tongs, knife and cutting board for chopping
+
+//2. Once everything is gathered you can begin the cooking process by:
+  //filling the pot with enough water to cover the Spagehtti
+    //add at least 3 tablespoons of salt to water and then bring to a boil
+    //while water is boiling, turn pan on low heat and add enough oil to cover the bottom of the pan
+
+//3. Chop Garlic and parsley
+  //cut the garlic into thin slices, no need to mince, chop or mash
+    //chop parsley roughly, no need to be precise on how much you use, a nice handful will work
+      //set aside
+
+//4. When water finally boils add pasta and stir occasionlly to prevent sticking
+
+//5. After pasta is in, start the sauce
+  //add garlic to pan, add enough chili flakes to suit your taste and turn up heat from low to medium low
+    //small bubbles should be seen around the garlic, keep the heat low so these stay present but dont grow
+      //stir occasionlly
+
+//6. 8-12 minutes to cook Spagehtti,
+  //after 6 minutes fish a noodle out with tongs and take a bite or break with hands to check doneness
+    //repeat process every minute or so to ensure its done
+
+//7. when pasta is finished, fish out from pot using tongs and add directly to pan
+    //cut the heat
+      //do this so the pasta is still wet, adding pasta water to the sauce in the process
+          //water will stop the pasta from frying, prevent to garlic from overcooking and add body to sauce (starchy water).
+
+
+//8 Stir to combine
+  //top with parsley and drizzle of olive oil
+    //this is the step in where you would add the optional ingriendients: squeeze of lemon, pinch of pepper, grated cheese on top
+
+//9 Serve
+
+
+//CODE 2 STARTS HERE//
+
+
+//Homepage Video Intro
+
+//When page is opened
+  //show black screen with playbutton icon centered
+    //when clicked: start step 2
+//Step2: rectanlge appears from left of screen
+  //animation of rectangle going to center of screen
+    //When rectangle hovered, play button reappears
+      //when play button clicked: start step 3
+//Step3: Play button (vector graphic acting as hyperlink) when clicked:
+  //video player appears in rectangle
+    // after 3 seconds: start introduction to site Video
+      //after video ends start step 4
+//Step4: video player disappears
+  //show enter site button in its place
+    //when clicked:
+      //load homepage to website
