274ddb6b0cad6463d177efedb583a35e08832510
nrodarte <70286040+nrodarte@users.noreply.github.com>
2021-02-14 22:35:13 -0800

    JOJ

---
+++ lab5/js/lab.js
@@ -0,0 +1,20 @@
+// Author Nick R//
+//Created 2-14-2021//
+//License: Public Domain//
+
+
+//Defining Variables//
+make = "Buick";
+model = "Verano";
+color = "black";
+year = "2016";
+
+// calculation
+age = 2021 - year;
+
+//output
+document.writeIn("Make: " + make + "<br>");
+document.writeIn("Model: " + model + "<br>");
+document.writeIn("Color: " + color + "<br>");
+document.writeIn("Year: " + year + "<br>");
+document.writeIn("Age: " + age + " years<br>");
